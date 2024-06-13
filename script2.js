7efc33ffedd7fdfb21ec64ddd2bf8afca2c2539f
mabdulrahmanahmed <67257579+mabdulrahmanahmed@users.noreply.github.com>
2024-06-13 23:03:55 +0500

    Task-Manager (week 1)
    features:
    - Adding task
    - displays all tasks in the form of an unordered list
    - Currently no CSS present

---
+++ script2.js
@@ -0,0 +1,32 @@
+// var button = document.getElementById("but");
+
+// button.addEventListener("click", function addTask() {
+//     var input = document.getElementById("input");
+//     var task = input.value;
+//     var list = document.getElementById("list");
+
+//     var li = document.createElement("li");
+    
+//     // li.textContent = task;
+//     li.innerHTML=`${task}<br>`;
+//     list.appendChild(li);
+//     input.value = "";
+// });
+
+function addTask() {
+    var input = document.getElementById("input");
+    var task = input.value;
+    var list = document.getElementById("list");
+
+    if (task === ""){
+        alert("Please enter a task.");
+        return;
+    }
+
+    var li = document.createElement("li");
+    
+    li.textContent = input.value;
+    // li.innerHTML=`${task}<br>`;
+    list.appendChild(li);
+    input.value = "";
+}
