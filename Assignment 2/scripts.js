a7983c4e2134dccc938377ca5d6bfc6dcea82055
mabdulrahmanahmed <67257579+mabdulrahmanahmed@users.noreply.github.com>
2024-06-23 20:40:17 +0500

    Task Manager (Week 2)
    Features:
    - You can add a task
    - You can complete a task
    - You can delete a task
    - You can edit a task

--- Assignment 2/scripts.js
+++ Assignment 2/scripts.js
@@ -4,13 +4,12 @@ function showForm()
     form.style.display = "block";
 }
 
-
 function hideForm(){
     event.preventDefault();     // the button 'Back refresh hence dynamic list/ data lost
     var form = document.getElementById("task-form-container");
     form.style.display = "none";
+    resetForm();
 }
-
 
 function addTask()  //since all if conditions for input boxes are in addTask function which only triggers when the button is pressed so for example if I leave certain categories they will turn red. now technically speaking they should turn black once input is recognises but that only happens when the button is pressed hence it will remain red until the button is pressed regardless of whether it has input or not. the solution to this is to make a variable which becomes true the first time the button is pressed after wihch the if conditions should constantly run so that all updates are being done live and once a category is filled the border color will change back to normal even before pressing the button.
 {
@@ -69,11 +68,15 @@ function addTask()  //since all if conditions for input boxes are in addTask fun
         return;
     }
 
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox";
+    
     var taskContent = document.createElement("span");
-    taskContent.textContent = `${tname.value} - ${ddate.value}` ;
+    taskContent.textContent = `${tname.value} - ${tdes.value} - ${sdate.value} - ${ddate.value}`;
 
-    // var taskActions = document.createElement("div");     another method ... this method allowed for buttons 'delete' and 'edit' to be on a different line as a <div> class was being created which autmocatically creates a new line
-    // taskActions.className = "task-actions";
+    var taskActions = document.createElement("div");     // another method ... this method allowed for buttons 'delete' and 'edit' to be on a different line as a <div> class was being created which autmocatically creates a new line
+    taskActions.className = "task-actions";
 
     var editButton = document.createElement("button");
     editButton.id = "edit";
@@ -89,13 +92,22 @@ function addTask()  //since all if conditions for input boxes are in addTask fun
         li.remove();
     };
 
-    // taskActions.appendChild(editButton);
-    // taskActions.appendChild(deleteButton);
+    taskActions.appendChild(editButton);
+    taskActions.appendChild(deleteButton);
+    li.appendChild(checkbox);
     li.appendChild(taskContent);
-    li.appendChild(editButton);
-    li.appendChild(deleteButton);
-    // li.appendChild(taskActions);
+    // li.appendChild(editButton);
+    // li.appendChild(deleteButton);
+    li.appendChild(taskActions);
     ul.appendChild(li);
+
+    checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+          taskContent.style.textDecoration = "line-through";
+        } else {
+          taskContent.style.textDecoration = "none";
+        }
+    });
 
     tname.value = "";
     tdes.value = "";
@@ -103,4 +115,92 @@ function addTask()  //since all if conditions for input boxes are in addTask fun
     ddate.value = "";
     form.style.display = "none";
     
+}
+
+function resetForm() {
+    document.getElementById("tname").value = "";
+    document.getElementById("tdes").value = "";
+    document.getElementById("sdate").value = "";
+    document.getElementById("ddate").value = "";
+    document.getElementById("tname").style.borderBlockColor = "black";
+    document.getElementById("tdes").style.borderBlockColor = "black";
+    document.getElementById("sdate").style.borderBlockColor = "black";
+    document.getElementById("ddate").style.borderBlockColor = "black";
+    document.getElementById("add-task-button").style.display = "inline";
+    document.getElementById("save-changes-button").style.display = "none";
+}
+
+function editTask(li, taskContent) {
+    var form = document.getElementById("task-form-container");
+    form.style.display = "block";
+
+    var tname = document.getElementById("tname");
+    var tdes = document.getElementById("tdes");
+    var sdate = document.getElementById("sdate");
+    var ddate = document.getElementById("ddate");
+
+    var taskDetails = taskContent.textContent.split(" - ");
+    tname.value = taskDetails[0];
+    tdes.value = taskDetails[1];
+    sdate.value = taskDetails[2];
+    ddate.value = taskDetails[3];
+
+    document.getElementById("add-task-button").style.display = "none";
+    document.getElementById("save-changes-button").style.display = "inline";
+
+    document.getElementById("save-changes-button").onclick = function() {
+        saveChanges(li, taskContent);
+    };
+}
+
+function saveChanges(li, taskContent) {
+    var tname = document.getElementById("tname");
+    var tdes = document.getElementById("tdes");
+    var sdate = document.getElementById("sdate");
+    var ddate = document.getElementById("ddate");
+
+    var condition1 = true, condition2 = true;
+
+    if (tname.value === "") {
+        tname.style.borderBlockColor = "red";
+        condition1 = false;
+    } else {
+        tname.style.borderBlockColor = "black";
+    }
+    if (tdes.value === "") {
+        tdes.style.borderBlockColor = "red";
+        condition1 = false;
+    } else {
+        tdes.style.borderBlockColor = "black";
+    }
+    if (sdate.value === "") {
+        sdate.style.borderBlockColor = "red";
+        condition1 = false;
+    } else {
+        sdate.style.borderBlockColor = "black";
+    }
+    if (ddate.value === "") {
+        ddate.style.borderBlockColor = "red";
+        condition1 = false;
+    } else {
+        ddate.style.borderBlockColor = "black";
+    }
+    if (ddate.value <= sdate.value) {
+        ddate.style.borderBlockColor = "red";
+        condition2 = false;
+    } else {
+        ddate.style.borderBlockColor = "black";
+    }
+
+    if (condition1 === false) {
+        alert("Please fill all the categories.");
+        return;
+    } else if (condition2 === false) {
+        alert("Due Date must be after Start Date.");
+        return;
+    }
+
+    taskContent.textContent = `${tname.value} - ${tdes.value} - ${sdate.value} - ${ddate.value}`;
+    resetForm();
+    document.getElementById("task-form-container").style.display = "none";
 }
